62a7e9e601dee831792973ea6c4711bfd0ebcd3f
Igor Câmara <igor.camara@datasinapse.com.br>
2025-09-27 18:41:33 -0300

    chore(settings): adicionando configurações de banco e cors

--- clients/holder/server/src/api/__init__.py
+++ clients/holder/server/src/api/__init__.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 load_dotenv()
@@ -9,11 +10,18 @@ class Settings:
         self.port = int(os.getenv("PORT", 8000))
         self.debug = os.getenv("DEBUG", "true").lower() == "true"
 
-        self.admin_url = os.getenv("ADMIN_URL", "http://localhost:8021")
+        self.cors_origins = os.getenv("CORS_ORIGINS", "*").split(",")
+
+        self.admin_url = os.getenv("ADMIN_URL", "http://localhost:8031")
         self.api_key = os.getenv("API_KEY", "mysecretapikey")
 
         self.jwt_secret_key = os.getenv("JWT_SECRET_KEY", "myjwtsecretkey")
         self.jwt_algorithm = os.getenv("JWT_ALGORITHM", "HS256")
         self.access_token_expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+        BASE_DIR = Path(__file__).resolve().parents[5]
+        DB_PATH = BASE_DIR / "db" / "holder.db"
+        
+        self.database_url = os.getenv("DATABASE_URL", f"sqlite:///{DB_PATH}")
 
 settings = Settings()
