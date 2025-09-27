77bb002e3a0c7d7e8b546131859de5608e611a1c
Igor Câmara <igor.camara@datasinapse.com.br>
2025-09-27 18:46:02 -0300

    fix(headers): uso de alias para retorno na resposta da api

--- clients/holder/server/src/api/schemas/headers.py
+++ clients/holder/server/src/api/schemas/headers.py
@@ -2,6 +2,6 @@ from typing import Optional, Dict, Any
 from pydantic import BaseModel, Field
 
 class AuthHeaders(BaseModel):
-    token: Optional[str] = Field(None, example="Bearer your_jwt_token_here")
-    did: Optional[str] = Field(None, example="did:example:123456789abcdefghi")
-    verkey: Optional[str] = Field(None, example="your_verification_key_here")
+    token: Optional[str] = Field(None, example="Bearer your_jwt_token_here", alias="authorization")
+    did: Optional[str] = Field(None, example="did:example:123456789abcdefghi", alias="x-did")
+    verkey: Optional[str] = Field(None, example="your_verification_key_here", alias="x-verkey")
