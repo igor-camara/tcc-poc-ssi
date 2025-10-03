766ff662305dea13f627e0309d7fc3080bdb7c8d
Igor Câmara <igor.camara@datasinapse.com.br>
2025-10-02 23:21:56 -0300

    feat(model): criação do model para iniciar a criação de solicitação de oferta de credencial

--- clients/issuer/server/src/modules/credential/schema.py
+++ clients/issuer/server/src/modules/credential/schema.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from modules.utils.model import SuccessResponse
+from typing import Dict, Any
 
 class CreateCredentialRequest(BaseModel):
     name: str = Field(..., description="Name of the schema")
@@ -11,4 +12,9 @@ class CredentialDetail(BaseModel):
     name: str = Field(..., description="Name of the credential")
     version: str = Field(..., description="Version of the credential")
     attrNames: list[str] = Field(..., description="List of attribute names")
-    
+
+class CredentialOfferRequest(BaseModel):
+    connection_id: str = Field(..., description="Connection ID with the holder")
+    cred_def_id: str = Field(..., description="Credential definition ID")
+    attributes: Dict[str, Any] = Field(..., description="Credential attributes as key-value pairs")
+    comment: str = Field(default="", description="Optional comment for the credential offer")
