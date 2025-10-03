20c85ec5872b20ac8c53de6631aec63a37d02cb0
Igor Câmara <igor.camara@datasinapse.com.br>
2025-10-02 22:04:11 -0300

    feat(model): criando model de retorno dos detalhes de schema

--- clients/issuer/server/src/modules/credential/schema.py
+++ clients/issuer/server/src/modules/credential/schema.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, Field
+from modules.utils.model import SuccessResponse
 
-class CredentialRequest(BaseModel):
+class CreateCredentialRequest(BaseModel):
     name: str = Field(..., description="Name of the schema")
     version: str = Field(..., description="Version of the schema")
     attributes: list[str] = Field(..., description="List of attribute names")
+
+class CredentialDetail(BaseModel):
+    id: str = Field(..., description="ID of the credential")
+    name: str = Field(..., description="Name of the credential")
+    version: str = Field(..., description="Version of the credential")
+    attrNames: list[str] = Field(..., description="List of attribute names")
+    
