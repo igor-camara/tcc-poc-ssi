fe5ef0f07af873c4d541f77869b2f378a7ec1499
Igor Câmara <igor.camara@datasinapse.com.br>
2025-10-02 22:05:02 -0300

    feat(service): consulta dos schemas e detalhes de schema

--- clients/issuer/server/src/modules/credential/service.py
+++ clients/issuer/server/src/modules/credential/service.py
@@ -1,5 +1,6 @@
 from typing import List
 from modules.utils.ssi import get_client
+from modules.credential.schema import CredentialDetail
 
 async def create_schema(schema_name: str, schema_version: str, attributes: List[str]) -> dict | str:
         try:
@@ -28,3 +29,29 @@ async def create_credential_definition(schema_id: str, support_revocation: bool 
             return cred_def_data
         except Exception as e:
             return "CRED_DEF_CREATION_FAILED"
+        
+async def list_ledger_credentials() -> List[dict] | str:
+    try:
+
+        public_did = await get_client().wallet.get_public_did()
+        did_info = public_did.to_dict() if hasattr(public_did, 'to_dict') else public_did
+
+        result = await get_client().schema.get_created_schemas(schema_issuer_did=did_info['result']['did'])
+
+        credentials = [cred.to_dict() if hasattr(cred, 'to_dict') else cred for cred in result]
+        return credentials
+    except Exception as e:
+        return "CREDENTIAL_RETRIEVAL_FAILED"
+    
+async def get_credential_by_id(credential_id: str) -> dict | None:
+    try:
+        result = await get_client().schema.get_schema(schema_id=credential_id)
+        credential = result.to_dict() if hasattr(result, 'to_dict') else result
+        return CredentialDetail(
+            id=credential['schema']['id'],
+            name=credential['schema']['name'],
+            version=credential['schema']['version'],
+            attrNames=credential['schema']['attrNames']
+        ) if credential else None
+    except Exception as e:
+        return None
